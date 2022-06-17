1d6e3cf022bdd1a95ad40af17a3b6e7ec88d6536
Lakshay Maini <62194911+lakshh07@users.noreply.github.com>
2022-06-18 00:41:17 +0530

    changed network config in rainbow

--- helpers/rainbowSetup.js
+++ helpers/rainbowSetup.js
@@ -1,16 +1,16 @@
 import { getDefaultWallets } from "@rainbow-me/rainbowkit";
-import { chain, configureChains, createClient, WagmiConfig } from "wagmi";
+import { chain, configureChains, createClient } from "wagmi";
 
 import { alchemyProvider } from "wagmi/providers/alchemy";
 import { publicProvider } from "wagmi/providers/public";
 
 export const { chains, provider } = configureChains(
-  [chain.mainnet, chain.polygonMumbai, chain.polygon],
+  [chain.polygonMumbai],
   [alchemyProvider({ alchemyId: process.env.ALCHEMY_ID }), publicProvider()]
 );
 
 const { connectors } = getDefaultWallets({
-  appName: "DeBooki",
+  appName: "BlockHash",
   chains,
 });
 
